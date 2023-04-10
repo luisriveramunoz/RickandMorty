622a6c9e3e8e86434b740b00b1e566d6fd1feefe
Luis <luis@Luiss-iMac.local>
2023-04-10 16:07:14 -0600

    agrege algo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+
+import React, {useEffect, useState} from "react"
+import Navbar from "./components/Navbar";
+import Characters from "./components/Characters";
+import Pagination from "./components/Pagination";
+
+
+
 
 function App() {
+  const [characters, setCharacters] = useState([])
+  const [info, setInfo] = useState({})
+  const initialUrl = "https://rickandmortyapi.com/api/character"
+  const fetchCharacters = (url) => {
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      setCharacters(data.results)
+      setInfo(data.info)
+    })
+    .catch(error => console.log(error))
+
+  }
+  const onPrevious = () => {
+    fetchCharacters(info.prev);
+  }
+  const onNext = () => {
+    fetchCharacters(info.next)
+  }
+
+  useEffect(() => {
+    fetchCharacters(initialUrl)
+  },[])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <Navbar brand="Rick and morty app"/>
+    <Pagination prev={info.prev} next={info.next} onPrevious={onPrevious} onNext={onNext}/>
+    <div className="container mt-5" >
+      <Characters characters={characters}/>
     </div>
-  );
+    <Pagination prev={info.prev} next={info.next} onPrevious={onPrevious} onNext={onNext}/>
+    </>
+    
+    );
 }
 
 export default App;
